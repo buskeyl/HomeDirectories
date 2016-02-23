c41dbc2bada932793bce022366e09c06b0220f25
buskeyl <commerce@buskey.com>
2016-02-23 07:48:48 -0500

    Added files via upload

---
+++ FixAclForTest.ps1
@@ -0,0 +1,5 @@
+.\set-owner.ps1
+Set-Owner -path \\spablskxa8r01\homedrives\lee.buskey.ctr\outbound\test.txt -account sked\lee.buskey.ctr
+Set-Owner -path \\spablskxa8r01\homedrives\lee.buskey.ctr\outbound\test.xls -account sked\lee.buskey.ctr
+Set-Owner -path \\spablskxa8r01\homedrives\lee.buskey.ctr\outbound\test.xlsx -account sked\lee.buskey.ctr
+Set-Owner -path \\spablskxa8r01\homedrives\lee.buskey.ctr\outbound\test.pdf -account sked\lee.buskey.ctr
